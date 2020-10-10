ef5cca16ce18eeffca60981b7f7e68c8cfeb73ad
Anh <an7s@virginia.edu>
2020-10-10 16:14:29 -0700

    Add pgk-config

--- get-packages.sh
+++ get-packages.sh
@@ -7,7 +7,7 @@ echo "Intalling required packages for STG-Q"
 echo "====================================="
 
 LLVM_PKGS="libllvm-10-ocaml-dev libllvm10 llvm-10 llvm-10-dev llvm-10-doc llvm-10-examples llvm-10-runtime"
-OTHER_PKGS="uuid-dev libz-dev"
+OTHER_PKGS="uuid-dev libz-dev pkg-config"
 JAVA_PKGS="antlr4"
 REALPAVER_PKGS="flex bison"
 
