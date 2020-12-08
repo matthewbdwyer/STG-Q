8fa0b7cb516d1e5a2904f908728b3d8d182f9e61
Anh <an7s@virginia.edu>
2020-12-07 16:44:22 -0800

    Add output directory to dedup script

--- src/tools/stgred.sh
+++ src/tools/stgred.sh
@@ -4,16 +4,32 @@ STGDIFF=${STGQ_BIN}/stgdiff.sh
 
 shopt -s nullglob
 files=($1/*.stg)
+declare -A uniques
+
+if [ ! -d "$2" ]; then
+	mkdir "$2"
+fi
 
 for ((i=0; i<${#files[@]}; i++)); 
 do
+  duplicate="no"
   for ((j=i+1; j<${#files[@]}; j++)); 
   do
     ${STGDIFF} "${files[$i]}" "${files[$j]}" >/dev/null;
     if [ $? -eq 0 ];
     then
+      # print out duplicates
       echo "$(basename -- "${files[$j]}")"
+      duplicate="yes"
     fi
   done
+  if [ "$duplicate" == "no" ]; then
+	  name=$(basename -- "${files[$i]}")
+	  if [ -d $2 ]; then
+	  echo "$name is unique -- copying into $2"
+	  cp ${files[$i]} $2
+	  uniques[$name]=1
+  	fi
+  fi 
 done
 
