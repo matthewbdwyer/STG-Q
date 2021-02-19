8c45caa2d329bbf84b6ac3e49fc3eb062677681d
Anh <an7s@virginia.edu>
2021-02-18 19:18:03 -0800

    WIP: CI/CD

---
+++ scripts/distclean.sh
@@ -0,0 +1,23 @@
+# 
+# Utility script to cleanup STG-Q
+#
+
+if [ -z "$STGQ_HOME" ]; then
+	echo "Environment variable STGQ_HOME is undefined"
+	echo "Did you forget to source set_env_vars?"
+	exit 1
+fi
+
+cd $STGQ_HOME
+if [ -d build ]; then
+	cd build
+	make clean
+fi
+
+if [ -d "$STGQ_BIN" ]; then
+	cd $STGQ_BIN
+	rm stg*
+	rm Quantify.sh
+	rm comb
+	rm res
+fi
