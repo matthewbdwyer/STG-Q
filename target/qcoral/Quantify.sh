b0cf390c305b9e9cac2698ad65d0a575a296d9cc
Rishab <rb6xj@virginia.edu>
2021-02-28 21:58:50 -0500

    closes #5; Included num_samples

--- target/qcoral/Quantify.sh
+++ target/qcoral/Quantify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # qcoral_path=${2:-/home/rishab/Music/qcoral}
-qcoral_path=$QCORAL_HOME/qcoral
+qcoral_path=$STGQ_HOME/qcoral
 printf "\nNOTE: Intermediate files will be stored in /tmp/QCounter\n";
 if [ ! -d /tmp/QCounter ]
 	then
@@ -31,28 +31,37 @@ no_out+=1
 
 folder_path=$(readlink -f $1)
 dictionary_path=""
+num_samples=0
+
+
 if [ $2 ]; then
 	dictionary_path=$(readlink -f $2)
 else
 	dictionary_path="None"
 fi
 
+if [ $3 ]; then
+	num_samples=$3
+else
+	num_samples=5000000
+fi
+
 files=$folder_path/*
 
-#cd $STGQ_HOME/build/src/tools 																	#Changed this
+cd $STGQ_HOME/build/src/tools 																	#Changed this
 declare -i nof=0
 for file in $files
 do
   if [[ "$file" == *".stg" ]]; then
 	nof+=1
-	stgpp "$folder_path/$(basename "$file")" > "/tmp/QCounter/stg/${nof}.stg"
+	./stgpp "$folder_path/$(basename "$file")" > "/tmp/QCounter/stg/${nof}.stg"
   fi
 
 done
 
 
 cd $OLDPWD
-#cd $STGQ_HOME/build/target/qcoral
+cd $STGQ_HOME/build/target/qcoral
 files=/tmp/QCounter/stg/*
 
 nof=0
@@ -60,9 +69,9 @@ for file in $files
 do
 	nof+=1
 	if [[ "$dictionary_path" == "None" ]];then
-		stg2qc "/tmp/QCounter/stg/$(basename "$file")" > "/tmp/QCounter/qc/${nof}.qcoral"
+		./stg2qc "/tmp/QCounter/stg/$(basename "$file")" > "/tmp/QCounter/qc/${nof}.qcoral"
 	else
-		stg2qc "/tmp/QCounter/stg/$(basename "$file")" "$dictionary_path" > "/tmp/QCounter/qc/${nof}.qcoral"
+		./stg2qc "/tmp/QCounter/stg/$(basename "$file")" "$dictionary_path" > "/tmp/QCounter/qc/${nof}.qcoral"
 	fi
 done
 
@@ -75,11 +84,11 @@ do
 	arr+=' '
 done
 
-comb $arr
+./comb $arr
 cd $OLDPWD
 pw=$pwd
 cd $qcoral_path
-./run_qcoral.sh --mcIterativeImprovement --mcProportionalBoxSampleAllocation --mcSeed 123456 --mcMaxSamples 5000000 --mcInitialPartitionBudget 50000 --mcTargetVariance 1E-20 --mcSamplesPerIncrement 10000 "/tmp/QCounter/comb.qcoral" > "/tmp/QCounter/out/Result_${no_out}.out"
+./run_qcoral.sh --mcIterativeImprovement --mcProportionalBoxSampleAllocation --mcSeed 123456 --mcMaxSamples $num_samples --mcInitialPartitionBudget 50000 --mcTargetVariance 1E-20 --mcSamplesPerIncrement 10000 "/tmp/QCounter/comb.qcoral" > "/tmp/QCounter/out/Result_${no_out}.out"
 
 tail -1 "/tmp/QCounter/out/Result_${no_out}.out"
 printf "\n"
